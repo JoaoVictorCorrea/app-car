a5cd7b5b1050809b7491d8a0aa2b26889bed6f29
João Victor Correa <correa.joaov25@gmail.com>
2024-02-05 15:48:12 -0300

    closes #2 - Componente Pai (Tabela)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CarsComponent } from './components/cars/cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
