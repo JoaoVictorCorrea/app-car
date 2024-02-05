72ca9c1006c5b1fe7d27d603ee2e56a43e175385
João Victor Correa <correa.joaov25@gmail.com>
2024-02-05 16:52:28 -0300

    closes #4 - Inserção de Dados

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Car } from '../../car';
 
 @Component({
   selector: 'app-cars',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
+  //data
+  cars: Car[] = [
+    {
+      id: 1,
+      name: "Palio",
+      automaker: "Fiat",
+      price: 25000,
+      year: 2000
+    },
+    {
+      id: 2,
+      name: "Fox",
+      automaker: "Volkswagen",
+      price: 50000,
+      year: 2015
+    },
+    {
+      id: 3,
+      name: "Versa",
+      automaker: "Nissan",
+      price: 75000,
+      year: 2019
+    },
+  ]
 }
